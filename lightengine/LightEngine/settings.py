47f9f45e3dd6847d7fa7d32688ce0fe0032bca42
BuildTools <mjako676@gmail.com>
2020-05-06 14:29:49 +0300

    Trying to get it to work lol

--- lightengine/LightEngine/settings.py
+++ lightengine/LightEngine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'enzmjdn1jyf4rl6_hi1423)u^i5n@yidvgm7@ob=enmxjkejxq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://lightengine.herokuapp.com/"]
 
 
 # Application definition
@@ -122,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
