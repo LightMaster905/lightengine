732732d672c1f1251c232ae9bfa203120ca9e348
BuildTools <mjako676@gmail.com>
2020-06-07 11:13:12 +0300

    Fixed wsgi.py

--- lightengine/LightEngine/wsgi.py
+++ lightengine/LightEngine/wsgi.py
@@ -12,6 +12,6 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LightEngine.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
-from configurations.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
